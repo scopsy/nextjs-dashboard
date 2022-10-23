e0da90342fa313153c5e0aac98464507eeec7e96
Dima Grossman <dima@grossman.io>
2022-10-23 17:00:27 +0300

    remove unused nav items

--- src/layout/AdminLayout/Header/HeaderNotificationNav.tsx
+++ src/layout/AdminLayout/Header/HeaderNotificationNav.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faList } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-regular-svg-icons'
 import { Nav } from 'react-bootstrap'
 
 export default function HeaderNotificationNav() {
@@ -9,16 +8,6 @@ export default function HeaderNotificationNav() {
       <Nav.Item>
         <Nav.Link className="p-2">
           <FontAwesomeIcon icon={faBell} size="lg" />
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link className="p-2">
-          <FontAwesomeIcon icon={faList} size="lg" />
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link className="p-2">
-          <FontAwesomeIcon icon={faEnvelope} size="lg" />
         </Nav.Link>
       </Nav.Item>
     </Nav>
